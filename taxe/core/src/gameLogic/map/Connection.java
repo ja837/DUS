2e204b35986046a34ef3aae68ad061db883ded18
Michael <m1chael0@hotmail.co.uk>
2015-01-29 11:20:00 +0000

    Added blocked attribute to Connection

--- taxe/core/src/gameLogic/map/Connection.java
+++ taxe/core/src/gameLogic/map/Connection.java
@@ -3,18 +3,37 @@ package gameLogic.map;
 public class Connection {
 	private Station station1;
 	private Station station2;
-	//Add distances to connections?
-	//Add obstacle attribute to connection
+	private int blocked;
+
 	public Connection(Station station1, Station station2) {
 		this.station1 = station1;
 		this.station2 = station2;
+		this.blocked = 0;
 	}
-	
+
 	public Station getStation1() {
 		return this.station1;
 	}
 
 	public Station getStation2() {
 		return this.station2;
+	}
+
+	public boolean isBlocked() {
+		if (this.blocked > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void decrementBlocked() {
+		if (this.blocked > 0) {
+			this.blocked--;
+		}
+	}
+
+	public void setBlocked(int turns) {
+		this.blocked = turns;
 	}
 }
