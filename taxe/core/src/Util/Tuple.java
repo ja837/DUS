dfe3607f7356b6f6cb5223fdc178b960ed41d9ed
Ezard <ezard.ben@gmail.com>
2015-02-09 06:53:15 +0000

    Added in functionality to show connections on a route that the train has already passed along

--- taxe/core/src/Util/Tuple.java
+++ taxe/core/src/Util/Tuple.java
@@ -1,5 +1,8 @@
 package Util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tuple<T,U> {
     private T val1;
     private U val2;
@@ -9,7 +12,23 @@ public class Tuple<T,U> {
         this.val2 = val2;
     }
 
-    public T getFirst() {
+	public static <T, U> ArrayList<T> getFirstsFromList(List<Tuple<T, U>> list) {
+		ArrayList<T> al = new ArrayList<T>();
+		for (Tuple<T, U> t : list) {
+			al.add(t.getFirst());
+		}
+		return al;
+	}
+
+	public static <T, U> ArrayList<U> getSecondsFromList(List<Tuple<T, U>> list) {
+		ArrayList<U> al = new ArrayList<U>();
+		for (Tuple<T, U> t : list) {
+			al.add(t.getSecond());
+		}
+		return al;
+	}
+
+	public T getFirst() {
         return val1;
     }
 
