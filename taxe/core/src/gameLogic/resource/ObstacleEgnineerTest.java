c22d856a2f54c6ac66b6c849317ec83001675d11
ram534 <ram534@york.ac.uk>
2015-02-17 19:31:45 +0000

    test

---
+++ taxe/core/src/gameLogic/resource/ObstacleEgnineerTest.java
@@ -0,0 +1,28 @@
+package gameLogic.resource;
+
+import gameLogic.map.Connection;
+import gameLogic.map.Position;
+import gameLogic.map.Station;
+import junit.framework.TestCase;
+
+public class ObstacleEgnineerTest extends TestCase {
+
+    public void testUse() throws Exception {
+
+        Station station1 = new Station("station1", new Position(6, 2));
+        Station station2 = new Station("station2", new Position(4, 2));
+        Connection connection = new Connection(station1, station2);
+
+        assertEquals(false, connection.isBlocked());
+
+        Obstacle obstacle = new Obstacle();
+        obstacle.use(connection);
+
+        assertEquals(true, connection.isBlocked());
+
+        Engineer engineer = new Engineer();
+        engineer.use(connection);
+
+        assertEquals(false, connection.isBlocked());
+    }
+}
