5384d61cb6de3648bad0bc9e38706274386baa53
ram534 <ram534@york.ac.uk>
2015-02-17 19:32:24 +0000

    test

---
+++ taxe/core/src/gameLogic/resource/ResourceManagerTest.java
@@ -0,0 +1,44 @@
+package gameLogic.resource;
+
+import gameLogic.Player;
+import gameLogic.PlayerManager;
+import junit.framework.TestCase;
+
+public class ResourceManagerTest extends TestCase {
+    ResourceManager rm = new ResourceManager();
+
+    public void testGetRandomTrain() throws Exception {
+        Train train1 = rm.getRandomTrain();
+        System.out.println(train1.toString());
+
+        Train train2 = rm.getRandomTrain();
+        System.out.println(train2.toString());
+
+        Train train3 = rm.getRandomTrain();
+        System.out.println(train3.toString());
+
+        Train train4 = rm.getRandomTrain();
+        System.out.println(train4.toString());
+
+        Train train5 = rm.getRandomTrain();
+        System.out.println(train5.toString());
+
+        Train train6 = rm.getRandomTrain();
+        System.out.println(train6.toString());
+
+
+    }
+
+    public void testAddRandomResourceToPlayer() throws Exception {
+        PlayerManager playerManager = new PlayerManager();
+        playerManager.createPlayers(2);
+        Player player1 = playerManager.getCurrentPlayer();
+
+        rm.addRandomResourceToPlayer(player1);
+        rm.addRandomResourceToPlayer(player1);
+        rm.addRandomResourceToPlayer(player1);
+
+        assertEquals(3, player1.getResources().size());
+
+    }
+}
