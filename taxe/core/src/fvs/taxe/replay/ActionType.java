55948e762934722a8a4ee58dcda1bced00294ad6
Jamie Adkins <ja837@york.ac.uk>
2015-04-04 15:22:18 +0100

    Revert "Moved action to to within action class"
    This reverts commit c20f432bc643129cbd424e4cbcbe3c59c307a424.

---
+++ taxe/core/src/fvs/taxe/replay/ActionType.java
@@ -0,0 +1,16 @@
+package fvs.taxe.replay;
+
+/*
+ * Enum for which type of action was performed.
+ * Button for if a dialog button was clicked
+ * stage for if the stage was clicked
+ * Actor for if an actor was clicked.
+ */
+public enum ActionType {	
+	
+	BUTTON,
+	STAGE,
+	ACTOR,
+	
+
+}
